d72e2dbf3368acf0f849b1ae1000ac5969b2e2bb
amatiroo <105496747+amatiroo@users.noreply.github.com>
2024-01-04 15:16:57 -0600

    Add files via upload

--- Netflix.java
+++ Netflix.java
@@ -1,113 +1,194 @@
 import java.io.*;
 import java.util.Scanner;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import java.util.Vector;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.fs.Path;
 
+class DataOne implements Writable {
+    public int movieID;
+    public int userID;
+    public int rating;
+    public String year;
 
-import static java.lang.Integer.parseInt;
+    DataOne() {}
 
+    DataOne(int n, int d, int a, String z) {
+        movieID = n; userID = d; rating = a; year = z;
+    }
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(movieID);
+        out.writeInt(userID);
+        out.writeInt(rating);
+        out.writeUTF(year);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        movieID = in.readInt();
+        userID = in.readInt();
+        rating = in.readInt();
+        year = in.readUTF();
+    }
+}
+
+class DataTwo implements Writable {
+    public int movieID;
+    public String year;
+    public String title;
+
+    DataTwo() {}
+
+    DataTwo(int a, String n, String d) {
+        movieID = a; year = n; title = d;
+    }
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(movieID);
+        out.writeUTF(year);
+        out.writeUTF(title);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        movieID = in.readInt();
+        year = in.readUTF();
+        title = in.readUTF();
+    }
+}
+
+class Result implements Writable {
+
+    public String year;
+    public String movieTitle;
+
+    Result(String yr, String mt) {
+        year = yr; movieTitle = mt;
+    }
+
+    public void write(DataOutput out) throws IOException {
+
+        out.writeUTF(year);
+        out.writeUTF(movieTitle);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+
+        year = in.readUTF();
+        movieTitle = in.readUTF();
+    }
+
+    public String toString() {
+        return year + ": " + movieTitle;
+    }
+}
+
+class DataOneTwo implements Writable {
+    public short tag;
+    public DataOne data1;
+    public DataTwo data2;
+
+    DataOneTwo() {}
+
+    DataOneTwo(DataOne e) {
+        tag = 0; data1 = e;
+    }
+
+    DataOneTwo(DataTwo d) {
+        tag = 1; data2 = d;
+    }
+
+    public void write(DataOutput out) throws IOException {
+        out.writeShort(tag);
+        if (tag == 0)
+            data1.write(out);
+        else
+            data2.write(out);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        tag = in.readShort();
+        if (tag == 0) {
+            data1 = new DataOne();
+            data1.readFields(in);
+        } else {
+            data2 = new DataTwo();
+            data2.readFields(in);
+        }
+    }
+}
 
 public class Netflix {
-    //Below is the mapper function for counting the number of reviews for each movie
-    public static class ReviewsMapper extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+    public static class DataOneMapper extends Mapper<Object,Text,IntWritable,DataOneTwo > {
         @Override
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            String[] parts = value.toString().split(",");
-            if (parts.length >= 4) {
-                int movieID = Integer.parseInt(parts[0]); // taking just movie id
-                int userID = Integer.parseInt(parts[1]); // taking just userID
-                context.write(new IntWritable(movieID), new IntWritable(userID));
-            }
+        public void map ( Object key, Text value, Context context )
+                throws IOException, InterruptedException {
+            Scanner s = new Scanner(value.toString()).useDelimiter(",");
+            DataOne e = new DataOne(s.nextInt(),s.nextInt(),s.nextInt(),s.next());
+            context.write(new IntWritable(e.movieID),new DataOneTwo(e));
+            s.close();
         }
     }
-    //Below is the reducer function for counting the number of reviews for each movie
-    public static class ReviewsReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
-        @Override
-        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int total = 0;
 
-            for (IntWritable rating : values) {
-                total += 1;
+    public static class DataTwoMapper extends Mapper<Object,Text,IntWritable,DataOneTwo > {
+        @Override
+        public void map ( Object key, Text value, Context context )
+                throws IOException, InterruptedException {
+            Scanner s = new Scanner(value.toString()).useDelimiter(",");
+            DataTwo d = new DataTwo(s.nextInt(),s.next(),s.next());
+            context.write(new IntWritable(d.movieID),new DataOneTwo(d));
+            s.close();
+        }
+    }
 
-            }
-            context.write(key, new IntWritable(total));
-        }
-    }
-    //below is the mapper function for getting the average review for a movie with year
-    // where key will be movie-YYYY
-    public static class AvgRatingMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+    public static class ResultReducer extends Reducer<IntWritable,DataOneTwo,Result,DoubleWritable> {
+        static Vector<DataOne> d1 = new Vector<DataOne>();
+        static Vector<DataTwo> d2 = new Vector<DataTwo>();
         @Override
-        public void map(LongWritable key, Text value, Context context1) throws IOException, InterruptedException {
-            String[] parts = value.toString().split(",");
-            if (parts.length >= 4) {
-                String yearString = parts[3].substring(0, 4); // getting just YYYY from YYYY-MM-DD
-                Text yearText = new Text(yearString);
-                Text ID = new Text(parts[0] + "-" + yearText.toString());
-                double ratings = Double.parseDouble(parts[2]);
-                context1.write(new Text(ID), new DoubleWritable(ratings));
-            }
-        }
-    }
-    //below is the reudcer function for getting the avg review for a movie with year.
-    public static class AvgRatingReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
-        @Override
-        public void reduce(Text key, Iterable<DoubleWritable> values, Context context1) throws IOException, InterruptedException {
-            int total = 0;
+        public void reduce ( IntWritable key, Iterable<DataOneTwo> values, Context context )
+                throws IOException, InterruptedException {
+            d1.clear();
+            d2.clear();
+            for (DataOneTwo v : values)
+                if (v.tag == 0)
+                    d1.add(v.data1);
+                else d2.add(v.data2);
+
             double sum = 0.0;
+            int count = 0;
 
-            for (DoubleWritable rating : values) {
-                total += 1;
-                sum +=rating.get();
+            for (DataOne s : d1) {
+                sum += s.rating;
+                count++;
 
             }
-            double avgValue = Math.round((sum /total) * 1e7)/ 1e7; // avg rating value for each movie with YYYY
-            context1.write(new Text(key), new DoubleWritable(avgValue));
+            double avgrating = 0;
+            DataTwo d = d2.get(0);
+            if (count != 0) {
+                avgrating = Math.round((sum /count) * 1e7)/ 1e7;
+                context.write(new Result(d.year, d.title), new DoubleWritable(avgrating));
+
+            }
+
+
         }
     }
 
-    /* put your Map-Reduce methods here */
+    public static void main(String[] args) throws Exception {
 
-    public static void main ( String[] args ) throws Exception {
-        //the below job is to get th reviews for a movie count
         Job job = Job.getInstance();
-        job.setJobName("Project1 Reviews");
+        job.setJobName("Netflix rating project2");
         job.setJarByClass(Netflix.class);
         job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Result.class);
         job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(IntWritable.class);
-        job.setMapperClass(Netflix.ReviewsMapper.class);
-        job.setReducerClass(Netflix.ReviewsReducer.class);
-        job.setInputFormatClass(TextInputFormat.class);
+        job.setMapOutputValueClass(DataOneTwo.class);
+        job.setReducerClass(ResultReducer.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-        FileInputFormat.setInputPaths(job,new Path(args[0]));
-        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, DataOneMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, DataTwoMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.waitForCompletion(true);
-
-        // job 2 is for getting the average rating for the movie with Year.
-        Job job2 = Job.getInstance();
-        job2.setJobName("Project1 Avg Rating");
-        job2.setJarByClass(Netflix.class);
-        job2.setOutputKeyClass(Text.class);
-        job2.setOutputValueClass(DoubleWritable.class);
-        job2.setMapOutputKeyClass(Text.class);
-        job2.setMapOutputValueClass(DoubleWritable.class);
-        job2.setMapperClass(Netflix.AvgRatingMapper.class);
-        job2.setReducerClass(Netflix.AvgRatingReducer.class);
-        job2.setInputFormatClass(TextInputFormat.class);
-        job2.setOutputFormatClass(TextOutputFormat.class);
-        FileInputFormat.setInputPaths(job2,new Path(args[0]));
-        FileOutputFormat.setOutputPath(job2,new Path(args[2]));
-        job2.waitForCompletion(true);
-
     }
 }
